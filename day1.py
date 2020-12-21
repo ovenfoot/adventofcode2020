e1848a9b244d14928a95de6a1d3d20552d160749
ovenfoot <ovenfoot@hotmail.com>
2020-12-21 12:57:49 +0000

    dos2unix day1.py

--- day1.py
+++ day1.py
@@ -1,7 +1,6 @@
 import argparse
 
 
-
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("input-filename")
@@ -9,6 +8,8 @@ def main():
 	args = parser.parse_args()
 
 	print(args.input_filename)
+	print("i am doing things right now")
 
 if __name__ == "main":
+	print("sup")
 	main()
