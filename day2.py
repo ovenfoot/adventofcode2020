b739920675d5a8be59c2d58682e0276f1722f5b4
ovenfoot <ovenfoot@hotmail.com>
2020-12-21 13:34:06 +0000

    Finished parser

--- day2.py
+++ day2.py
@@ -1,16 +1,16 @@
 import argparse
 import pprint
 import logging
+import sys
 
-FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-logging.basicConfig(format=FORMAT)
+FORMAT = '[%(asctime)s] [%(levelname)s] %(message)s'
+logging.basicConfig(format=FORMAT, stream=sys.stdout, level=logging.DEBUG)
 
 TEST_PASSWORD = "1-3 b: cdefg"
 
 def parse_password_components(password_components_raw):
 	password_components = {}
 	password_components_raw_split = password_components_raw.split()
-
 
 	# Parse repeated spec
 	repeated_spec = password_components_raw_split[0].split("-")
@@ -32,8 +32,22 @@ def is_password_valid(password_components):
 	password = password_components["password"]
 
 	repeated_occurences = password.count(letter)
+
 	return (repeated_occurences >= lower_bound and repeated_occurences <= upper_bound)
 
+
+def count_valid_passwords(filename):
+	valid_count = 0
+	with open(filename, 'r') as file:
+		for line in file:
+			line = line.rstrip()
+			password_components = parse_password_components(line)
+			is_valid = is_password_valid(password_components)
+			logging.debug("\"{0}\" - valid? {1}".format(line, is_valid))
+			if is_valid is True:
+				valid_count = valid_count + 1
+
+	logging.info("There are {0} valid passwords".format(valid_count))
 
 
 def main():
@@ -42,7 +56,9 @@ def main():
 
 	args = parser.parse_args()
 
-	print("Examining {0}".format(args.input_filename))
+	logging.info("Examining file {0}".format(args.input_filename))
+
+	count_valid_passwords(args.input_filename)
 
 	# test_password_components = parse_password_components(TEST_PASSWORD)
 	# print(is_password_valid(test_password_components))
