d0e1df2cbe0787da7d4e2b36a0e713f60b3cc136
ovenfoot <ovenfoot@hotmail.com>
2020-12-21 19:37:12 +0000

    Setup basic logging infrastructure

--- java/src/com/ovenfoot/Main.java
+++ java/src/com/ovenfoot/Main.java
@@ -4,14 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.StreamHandler;
 import java.util.stream.Collectors;
 
 public class Main {
+    private static Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+        init();
         String filename = args[0];
         List<String> inputList = convertInputFileToStringList(filename);
-        System.out.println(inputList);
+//        System.out.println(inputList);
+        log.info(String.format("%s", inputList.toString()));
+    }
+
+    private static void init() {
+        log.setLevel(Level.ALL);
+        // Unknown why this logs to stderr as well
+        // log.addHandler(new StreamHandler(System.out, new SimpleFormatter()));
     }
 
     private static List<String> convertInputFileToStringList(String filePath) {
