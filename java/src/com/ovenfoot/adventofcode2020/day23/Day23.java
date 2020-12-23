56dd5f62ade363545e5fa1e59ff9b24a2b029670
ovenfoot <ovenfoot@hotmail.com>
2020-12-23 13:36:28 +0000

    Refactored how cups are placed

--- java/src/com/ovenfoot/adventofcode2020/day23/Day23.java
+++ java/src/com/ovenfoot/adventofcode2020/day23/Day23.java
@@ -12,54 +12,126 @@ public class Day23 {
         List<Integer> cups = getCupListFromInput(input);
         Integer maxCupValue = cups.stream().mapToInt(v -> v).max().orElseThrow(NoSuchElementException::new);
         Integer minCupValue = cups.stream().mapToInt(v -> v).min().orElseThrow(NoSuchElementException::new);
+        logging.info(String.format("Min cup %d, Max cup %d", minCupValue, maxCupValue));
+
+        Integer nthCup = 0;
         for (int i = 0; i < iterations; i++) {
             logging.info(String.format("-- move %d --", i + 1));
-            cups = playOneMovePartOne(cups, minCupValue, maxCupValue);
+            nthCup = playOneMovePartOne(cups, nthCup, minCupValue, maxCupValue);
         }
+        logging.info(String.format("Final: %s", cups));
 
     }
 
     public void testPart1() {
-        runPart1("389125467", 1);
+        runPart1("389125467", 10);
     }
 
-    public List<Integer> playOneMovePartOne(List<Integer> cups, Integer minCupValue, Integer maxCupValue) {
+    public void testPart2() {
+        runPart2("389125467", 10000000);
+    }
+
+    public void runPart2(String initialInput, Integer iterations) {
+        List<Integer> cups = initPart2(initialInput);
+        Integer maxCupValue = 1000000;
+        Integer minCupValue = 1;
+
+        Integer nthCup = 0;
+        for (int i = 0; i < iterations; i++) {
+            logging.info(String.format("-- move %d --", i + 1));
+            nthCup = playOneMovePartOne(cups, nthCup, minCupValue, maxCupValue);
+        }
+        logging.info(String.format("Final: %s", cups));
+        calculatePart2Score(cups);
+    }
+
+    public List<Integer> initPart2(String initialInput) {
+        List<Integer> cups = getCupListFromInput(initialInput);
+        for (int i = cups.size(); i <= 1000000; i++) {
+            cups.add(i);
+        }
+        return cups;
+    }
+
+    public Integer calculatePart2Score(List<Integer> cups) {
+        Integer pos = null;
+        for (int i = 0; pos == null; i++) {
+            if (cups.get(i) == 1) {
+                logging.info(String.format("Position of cup 1 is %d", i));
+                pos = i;
+            }
+        }
+        Integer clockWiseOne = cups.get((pos + 1) % cups.size());
+        Integer clockWiseTwo = cups.get((pos + 2) % cups.size());
+        Integer result = clockWiseOne * clockWiseTwo;
+        logging.info(String.format("The two cups clockwise of 1 are %d and %d, multiplying to give a result of %d",
+                clockWiseOne, clockWiseTwo, result));
+        return result;
+    }
+
+    public Integer playOneMovePartOne(List<Integer> cups, Integer nthMove,
+                                            Integer minCupValue, Integer maxCupValue) {
         logging.info(String.format("cups: %s", cups));
-        Integer currentCup = cups.get(0);
+        Integer currentCup = cups.get(nthMove);
         logging.info(String.format("current cup: %d", currentCup));
-        List<Integer> cupsInHand = placeCupsInHand(cups);
+        List<Integer> cupsInHand = placeCupsInHand(cups, nthMove);
         logging.info(String.format("pick up: %s", cupsInHand));
         Integer destinationCup = getDestinationCup(cups, cupsInHand, currentCup, maxCupValue, minCupValue);
         logging.info(String.format("destination: %d", destinationCup));
         cups = placeCupsFromHand(cups, cupsInHand, destinationCup);
         logging.info(String.format("result: %s", cups));
-        return cups;
+        return getIndexOfNextCup(cups, currentCup);
     }
 
+    public Integer getIndexOfNextCup(List<Integer> cups, Integer currentCup) {
+        for (int i = 0; i < cups.size(); i++) {
+            if (cups.get(i) == currentCup) {
+                return (i+1) % cups.size();
+            }
+        }
+        return null;
+    }
+
+    public static int findIndexOfCup(List<Integer> cups, Integer cupToFind) {
+        boolean found = false;
+        int index = 0;
+        for(int i = 0; !found; i++) {
+            if (cups.get(i) == cupToFind) {
+                found = true;
+                index = i;
+            }
+        }
+        return index;
+    }
     public static List<Integer> placeCupsFromHand(List<Integer> cupsNotInHand, List<Integer> cupsInHand,
                                                   Integer destination) {
 
-        boolean placed = false;
-        for (int i = 0; !placed; i++) {
-            if (cupsNotInHand.get(i) == destination) {
-                cupsNotInHand.addAll(i + 1, cupsInHand);
-                placed = true;
-            }
+        int i = findIndexOfCup(cupsNotInHand, destination);
+        for (int j = 0; j < cupsInHand.size(); j++) {
+            int position = (i + 1 + j) % (cupsInHand.size() + cupsNotInHand.size());
+            cupsNotInHand.add(position, cupsInHand.get(j));
         }
         return cupsNotInHand;
     }
 
-    public static List<Integer> placeCupsInHand(List<Integer> cups) {
+    public static List<Integer> placeCupsInHand(List<Integer> cups, Integer nthMove) {
         List<Integer> cupsInHand = new ArrayList<>();
-        cupsInHand.add(cups.remove(1));
-        cupsInHand.add(cups.remove(1));
-        cupsInHand.add(cups.remove(1));
+        for (int i = 0; i < 3; i++) {
+            if ((nthMove + 1) < cups.size()) {
+                cupsInHand.add(cups.remove(nthMove + 1));
+            } else {
+                cupsInHand.add(cups.remove(0));
+            }
+        }
         return cupsInHand;
     }
 
     public static Integer getDestinationCup(List<Integer> allCups, List<Integer> cupsInHand, Integer currentCup,
                                             Integer maxCup, Integer minCup) {
         Integer destinationCup = currentCup - 1;
+        if (destinationCup < minCup) {
+            destinationCup = maxCup;
+        }
         while (cupsInHand.contains(destinationCup) || destinationCup.equals(currentCup)) {
             destinationCup--;
             if (destinationCup < minCup) {
